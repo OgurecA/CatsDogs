3843612397a1363df3e4d6d901de47fada030a4c
OgurecA <acpwork044@gmail.com>
2024-07-25 16:48:59 +0400

    k

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Порт, на котором будет запущен сервер
+const PORT = process.env.PORT || 3000;
+
+// Обслуживание статических файлов из папки build
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Обработка запросов на корневой URL
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
