c134ba93d3920662ade13bf112a36ed02ee30b2b
OgurecA <acpwork044@gmail.com>
2024-07-26 15:58:54 +0400

    k

--- bot.js
+++ bot.js
@@ -4,7 +4,14 @@ const apiId = '25827801'; // Замените на ваш api_id
 const apiHash = '0d1a57becebd4d6bb7b3863e2090c32c'; // Замените на ваш api_hash
 const bot = new Telegraf('7491271001:AAEOiriYnXp_fFXVS_Iqvekzga6wSH0NxhU'); // Замените на токен вашего Telegram бота
 
-bot.start((ctx) => ctx.reply('Привет! Я ваш Telegram бот.'));
+bot.start((ctx) => {
+    ctx.reply(
+      'Привет! Я ваш Telegram бот. Авторизуйтесь через Telegram для доступа к вашему профилю',
+      Markup.inlineKeyboard([
+        Markup.button.url('Авторизоваться', 'https://yourwebsite.com')
+      ])
+    );
+  });
 bot.help((ctx) => ctx.reply('Отправьте мне что-нибудь!'));
 bot.on('text', (ctx) => {
   // Пример обработки текстового сообщения
