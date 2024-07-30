4b8ed9d88d3d68a5dd2fcac8e68c2102ce932536
OgurecA <acpwork044@gmail.com>
2024-07-30 15:55:22 +0400

    Ваше сообщение о коммите

--- bot.js
+++ bot.js
@@ -6,24 +6,15 @@ const bot = new TelegramBot(token, {polling: true});
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
-  const loginUrl = 'https://btc24news.online/telegram_auth'; // URL для авторизации
+  const loginUrl = 'https://t.me/PumpOrDump_bot/PumpOrDump'; // URL для авторизации
 
   const opts = {
     reply_markup: {
       inline_keyboard: [
-        [
-          {
-            text: 'Login',
-            login_url: {
-              url: loginUrl,
-              forward_text: 'Login (forwarded)' // Опционально: текст, который будет показан, когда ссылка будет переслана
-            }
-          },
-          {
-            text: 'Open Website',
-            url: 'https://btc24news.online'
-          }
-        ]
+        [{
+          text: 'Open Mini App',
+          web_app: { url: loginUrl }
+        }]
       ]
     }
   };
