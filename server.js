57fa849be88c2d45fa41cc8f0373292066179476
OgurecA <acpwork044@gmail.com>
2024-07-31 13:54:12 +0400

    k

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ app.get('/data', (req, res) => {
   });
 
 // Обработка любых маршрутов
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
