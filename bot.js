07eff61da644d2742145813b55365241d1ff2750
OgurecA <acpwork044@gmail.com>
2024-07-31 12:27:37 +0400

    k

--- bot.js
+++ bot.js
@@ -4,21 +4,23 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '7491271001:AAEOiriYnXp_fFXVS_Iqvekzga6wSH0NxhU';
 const bot = new TelegramBot(token, {polling: true});
 
+// Этот объект будет хранить временно ID для каждого пользователя
+const userSessions = {};
+
 bot.on('message', (msg) => {
-  const miniAppUrl = `https://t.me/PumpOrDump_bot/PupmOrDump?startapp=user_id_12345`;
-
-  const opts = {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: 'Open Mini App',
-            url: miniAppUrl  // Используем URL для mini app с параметром user_id
-          }
-        ]
-      ]
+    const chatId = msg.chat.id;
+    const text = msg.text;
+    
+    // Проверяем, если пользователь уже отправил ID
+    if (userSessions[chatId]) {
+        // Строим и отправляем ссылку с ID
+        const miniAppUrl = `https://t.me/PumpOrDump_bot/PumpOrDump?startapp=${text}`;
+        bot.sendMessage(chatId, `Вот ваша ссылка: ${miniAppUrl}`);
+        delete userSessions[chatId]; // Очищаем сессию после отправки ссылки
+    } else {
+        // Спрашиваем ID пользователя
+        bot.sendMessage(chatId, "Пожалуйста, введите ваш ID:");
+        userSessions[chatId] = true; // Сохраняем, что пользователь начал сессию
     }
-  };
-
-  bot.sendMessage(chatId, 'Click to open Mini App with your user ID!', opts);
 });
+
