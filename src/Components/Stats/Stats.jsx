8617597a46cee5aa7f5590982a4f9d754de033c8
OgurecA <acpwork044@gmail.com>
2024-08-01 18:19:40 +0400

    k

--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 import './Stats.css';
 import Count from '../Count/Count';
 import ProgressBar from '../Bar/Bar';
+import Head from '../Head/Head';
+
+import { HarrisH, TrumpH } from '../Pictures/Pictures';
 
 function Stats({ harrisPercentage, trumpPercentage }) {
     return (
       <div className="stats-container">
         <ProgressBar harrisPercentage={harrisPercentage} trumpPercentage={trumpPercentage} />
         <div className="head-bar">
-          <Count count={trumpPercentage} label={"Donald Trump"} className="TR"/>
+          <div className="name">Trump</div>
+          <Count count={trumpPercentage} className="TR"/>
         </div>
         <div className="head-bar">
-          <Count count={harrisPercentage} label={"Kamala Harris"} className="HS"/>
+          <div className="name">Harris</div>
+          <Count count={harrisPercentage} className="HS"/>
         </div>
       </div>
     );
