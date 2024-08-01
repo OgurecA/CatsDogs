826078bbc3458a6e15dbd44ba1989789d108d218
OgurecA <acpwork044@gmail.com>
2024-08-01 18:12:44 +0400

    k

--- src/Components/Count/Count.jsx
+++ src/Components/Count/Count.jsx
@@ -2,9 +2,10 @@
 import React from 'react';
 import './Count.css'; // Убедитесь, что подключили CSS файл для стилизации
 
-function Count({ count, className }) {
+function Count({ count, className, label }) {
     return (
         <div className={`count-container ${className}`}>
+            <h1>{label}</h1>
             {count}%
         </div>
     );
--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -2,21 +2,16 @@ import React from 'react'
 import './Stats.css';
 import Count from '../Count/Count';
 import ProgressBar from '../Bar/Bar';
-import Head from '../Head/Head';
-
-import { HarrisH, TrumpH } from '../Pictures/Pictures';
 
 function Stats({ harrisPercentage, trumpPercentage }) {
     return (
       <div className="stats-container">
         <ProgressBar harrisPercentage={harrisPercentage} trumpPercentage={trumpPercentage} />
         <div className="head-bar">
-          <Head src={TrumpH} className="trumpH"/>
-          <Count count={trumpPercentage} className="TR"/>
+          <Count count={trumpPercentage} label={"Donald Trump"} className="TR"/>
         </div>
         <div className="head-bar">
-          <Head src={HarrisH} className="harrisH"/>
-          <Count count={harrisPercentage} className="HS"/>
+          <Count count={harrisPercentage} label={"Kamala Harris"} className="HS"/>
         </div>
       </div>
     );
