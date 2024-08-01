a8d0dad4e0ff597101d850615d212d36f85325b1
OgurecA <acpwork044@gmail.com>
2024-08-01 18:30:03 +0400

    k

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
 
 
     useEffect(() => {
-        WebApp.setHeaderColor('#ff9100');
+        WebApp.setHeaderColor('#282c34');
         updateBar();
         const intervalId = setInterval(updateBar, 10000);
             if (WebApp.initDataUnsafe && WebApp.initDataUnsafe.user) {
