26ccbc168a0ba73e2a39256f2d19d9c5d90115d3
OgurecA <acpwork044@gmail.com>
2024-08-02 22:41:39 +0400

    k

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import ImageContainer from './Components/ImageContainer/ImageContainer';
 import BGcontainer from './Components/BGcontainer/BGcontainer';
