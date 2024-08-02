bb769007db83dc46fc3d595c83a75c01098673ba
OgurecA <acpwork044@gmail.com>
2024-08-02 09:31:08 +0400

    k

--- src/Components/Count/Count.jsx
+++ src/Components/Count/Count.jsx
@@ -2,9 +2,10 @@
 import React from 'react';
 import './Count.css'; // Убедитесь, что подключили CSS файл для стилизации
 
-function Count({ count, className }) {
+function Count({ count, className, label }) {
     return (
         <div className={`count-container ${className}`}>
+            <div className="label">{label}</div>
             {count}%
         </div>
     );
