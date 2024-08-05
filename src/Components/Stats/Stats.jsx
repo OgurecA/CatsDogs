a402b25acec9b69ad2306da9790c0ab12720ee67
OgurecA <acpwork044@gmail.com>
2024-08-05 15:57:44 +0400

    m

--- src/Components/Stats/Stats.jsx
+++ src/Components/Stats/Stats.jsx
@@ -8,10 +8,10 @@ function Stats({ harrisPercentage, trumpPercentage }) {
       <div className="stats-container">
         <ProgressBar harrisPercentage={harrisPercentage} trumpPercentage={trumpPercentage} />
         <div className="head-bar">
-          <Count count={trumpPercentage} className="TR" label={"Donald Trump"} />
+          <Count count={trumpPercentage} className="TR" label={"Snake"} />
         </div>
         <div className="head-bar">
-          <Count count={harrisPercentage} className="HS" label={"Kamala Harris"} />
+          <Count count={harrisPercentage} className="HS" label={"Owl"} />
         </div>
       </div>
     );
