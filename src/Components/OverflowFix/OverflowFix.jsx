c44150ccc5c45daa1c8b0d362a5e26b89a478c51
OgurecA <acpwork044@gmail.com>
2024-08-05 14:46:05 +0400

    m

--- src/Components/OverflowFix/OverflowFix.jsx
+++ src/Components/OverflowFix/OverflowFix.jsx
@@ -5,22 +5,18 @@ import ImageContainer from '../ImageContainer/ImageContainer';
 const OverflowFix = ({ harrisImage, trumpImage, onHarrisClick, onTrumpClick, isSelectedHarris, isSelectedTrump }) => {
     return (
         <div className="overflow-fix-container">
-            {(isSelectedHarris || isSelectedTrump) && (
-                <>
-                    <ImageContainer
-                        src={harrisImage}
-                        className={`image-container no-select harris-image ${isSelectedHarris ? 'selected' : ''}`}
-                        disable={false}
-                        onClick={onHarrisClick}
-                    />
-                    <ImageContainer
-                        src={trumpImage}
-                        className={`image-container no-select trump-image ${isSelectedTrump ? 'selected' : ''}`}
-                        disable={false}
-                        onClick={onTrumpClick}
-                    />
-                </>
-            )}
+            <ImageContainer
+                src={harrisImage}
+                className={`image-container no-select harris-image ${isSelectedHarris ? 'selected' : ''}`}
+                disable={false}
+                onClick={onHarrisClick}
+            />
+            <ImageContainer
+                src={trumpImage}
+                className={`image-container no-select trump-image ${isSelectedTrump ? 'selected' : ''}`}
+                disable={false}
+                onClick={onTrumpClick}
+            />
         </div>
     );
 }
