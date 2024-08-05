c817855d0bd1d15daba7e1b16e377263a548e656
OgurecA <acpwork044@gmail.com>
2024-08-05 15:09:07 +0400

    m

--- src/Components/Pictures/Pictures.js
+++ src/Components/Pictures/Pictures.js
@@ -1,7 +1,7 @@
 import HarrisImg from "../Photoes/OwlCoin.png";
 import TrumpImg from "../Photoes/SnakeCoin.png";
 import HarrisBG from "../Photoes/Suit.jpg";
-import TrumpBG from "../Photoes/Wine.jpg";
+import TrumpBG from "../Photoes/Text.jpg";
 import TrumpH from "../Photoes/TrumpH.png";
 import HarrisH from "../Photoes/HarrisH.png";
 import TrumpP from "../Photoes/harris pres.png";
