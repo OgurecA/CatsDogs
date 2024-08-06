7779c693ed11e8fbe72a6accd1782fbf9796c00b
OgurecA <acpwork044@gmail.com>
2024-08-06 16:40:02 +0400

    m

---
+++ src/Components/LoadScreen/LoadScreen.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import './LoadScreen.css';
+
+function LoadScreen() {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 5000);
+
+    return () => clearTimeout(timer); // Очистка таймера при размонтировании компонента
+  }, []);
+
+  if (!visible) {
+    return null;
+  }
+
+  return (
+    <div className="load-screen">LoadScreen</div>
+  );
+}
+
+export default LoadScreen;
