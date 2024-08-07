ddd3a0656712e5e5006af9c7631e724b37ce9747
OgurecA <acpwork044@gmail.com>
2024-08-07 18:12:59 +0400

    m

--- src/Components/LoadScreenContainer/LoadScreenContainer.jsx
+++ src/Components/LoadScreenContainer/LoadScreenContainer.jsx
@@ -3,6 +3,16 @@ import './LoadScreenContainer.css'
 import LoadScreen from '../LoadScreen/LoadScreen';
 
 function LoadScreenContainer({ CatBack }) {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          const loadScreenContainer = document.querySelector('.load-screen-container');
+          if (loadScreenContainer) {
+            loadScreenContainer.classList.add('hidden');
+          }
+        }, 5300);
+    
+        return () => clearTimeout(timer); // Очистка таймера при размонтировании компонента
+      }, []);
   return (
     <div className='load-screen-container'>
         <LoadScreen src={CatBack}/>
