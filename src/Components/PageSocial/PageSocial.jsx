e695db174185e2c8d1f10d095030816e283c3771
OgurecA <acpwork044@gmail.com>
2024-08-08 18:19:01 +0400

    m

--- src/Components/PageSocial/PageSocial.jsx
+++ src/Components/PageSocial/PageSocial.jsx
@@ -2,17 +2,6 @@ import React from 'react';
 import './PageSocial.css';
 
 const PageSocial = ({ className }) => {
-    const handleButtonClick = (appUrl, webUrl) => {
-        // Сначала попытаемся открыть приложение
-        const newWindow = window.open(appUrl, '_blank');
-        
-        // Через 1 секунду перенаправляем на веб-версию, если приложение не открылось
-        setTimeout(() => {
-            if (newWindow && newWindow.closed) {
-                window.location.href = webUrl;
-            }
-        }, 1000);
-    };
     return (
         <div className={`page-social ${className}`}>
             <a className="social-item" href="tg://resolve?domain=telegram" target="_blank" rel="noopener noreferrer">
@@ -21,7 +10,7 @@ const PageSocial = ({ className }) => {
             <a className="social-item" href="medium://medium.com" target="_blank" rel="noopener noreferrer">
                 Medium
             </a>
-            <a className="social-item" onClick={() => handleButtonClick('vnd.youtube://www.youtube.com', 'https://youtube.com')} href="#">
+            <a className="social-item" href="vnd.youtube://www.youtube.com" target="_blank" rel="noopener noreferrer">
                 Youtube
             </a>
             <a className="social-item" href="web3://web3.com" target="_blank" rel="noopener noreferrer">
