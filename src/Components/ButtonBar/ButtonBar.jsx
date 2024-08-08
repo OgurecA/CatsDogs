6f52706fd23cdbe97e0361c7deace02a479a6f12
OgurecA <acpwork044@gmail.com>
2024-08-08 11:58:17 +0400

    m

--- src/Components/ButtonBar/ButtonBar.jsx
+++ src/Components/ButtonBar/ButtonBar.jsx
@@ -2,16 +2,25 @@ import React from 'react';
 import './ButtonBar.css';
 
 const ButtonBar = ({ onOpenShop, onShowSocialPage }) => {
+    const [chosenButton, setChosenButton] = useState(null);
+
+    const handleButtonClick = (index, callback) => {
+        setChosenButton(prevIndex => (prevIndex === index ? null : index));
+        if (callback) callback();
+    };
     return (
         <div className="button-bar">
-            <button className="button-bar-button" onClick={onOpenShop}>
+            <button className={`button-bar-button ${chosenButton === 0 ? 'chosen' : ''}`} onClick={() => handleButtonClick(0, null)}>
                 Магазин
             </button>
-            <button className="button-bar-button" onClick={onShowSocialPage}>
+            <button className={`button-bar-button ${chosenButton === 1 ? 'chosen' : ''}`} onClick={() => handleButtonClick(1, onShowSocialPage)}>
                 Социальные сети
             </button>
-            <button className="button-bar-button">
+            <button className={`button-bar-button ${chosenButton === 2 ? 'chosen' : ''}`} onClick={() => handleButtonClick(2, null)}>
                 Опция 3
+            </button>
+            <button className={`button-bar-button ${chosenButton === 3 ? 'chosen' : ''}`} onClick={() => handleButtonClick(3, null)}>
+                Опция 4
             </button>
         </div>
     );
--- src/Components/Pictures/Pictures.js
+++ src/Components/Pictures/Pictures.js
@@ -1,5 +1,5 @@
-import HarrisImg from "../Photoes/OwlCoin.png";
-import TrumpImg from "../Photoes/SnakeCoin.png";
+import HarrisImg from "../Photoes/Gorilla.png";
+import TrumpImg from "../Photoes/Snake.png";
 import HarrisBG from "../Photoes/Suit.jpg";
 import TrumpBG from "../Photoes/Text.jpg";
 import TrumpH from "../Photoes/TrumpH.png";
@@ -8,6 +8,11 @@ import TrumpP from "../Photoes/Cats.jpg";
 import HarrisP from "../Photoes/Rama.jpg";
 import bybit from "../Photoes/bybit.jpg";
 import CatBack from "../Photoes/CatBack.jpg";
+import Tiger from "../Photoes/Tiger.png";
+import Snake from "../Photoes/Snake.png";
+import Croc from "../Photoes/Croc.png";
+import Elephant from "../Photoes/Elephant.png";
+import Gorilla from "../Photoes/Gorilla.png";
 
 
-export {TrumpImg, TrumpBG, HarrisBG, TrumpH, HarrisH, HarrisImg, HarrisP, TrumpP, bybit, CatBack };
+export {TrumpImg, TrumpBG, HarrisBG, TrumpH, HarrisH, HarrisImg, HarrisP, TrumpP, bybit, CatBack, Gorilla, Tiger, Snake, Croc, Elephant };
