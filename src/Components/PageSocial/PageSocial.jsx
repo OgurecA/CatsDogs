560ebceacbac92ac2c814a24c9345d82deb093f4
OgurecA <acpwork044@gmail.com>
2024-08-09 12:40:16 +0400

    m

--- src/Components/PageSocial/PageSocial.jsx
+++ src/Components/PageSocial/PageSocial.jsx
@@ -1,21 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './PageSocial.css';
 
 const PageSocial = ({ className }) => {
     const [checkingLinks, setCheckingLinks] = useState([]);
     const [checkedLinks, setCheckedLinks] = useState([]);
 
-    // Загрузка состояния из LocalStorage при загрузке страницы
-    useEffect(() => {
-        const savedCheckedLinks = JSON.parse(localStorage.getItem('checkedLinks')) || [];
-        setCheckedLinks(savedCheckedLinks);
-    }, []);
-
-    // Функция для обработки клика по кнопке
     const handleLinkClick = (index, event) => {
         event.preventDefault();
 
-        // Проверяем, есть ли уже класс checked
+        // Открываем ссылку сразу
+        window.open(event.target.href, '_blank');
+
         if (checkedLinks.includes(index)) {
             return; // Если класс checked уже есть, выходим из функции
         }
@@ -25,18 +20,15 @@ const PageSocial = ({ className }) => {
             setCheckingLinks(prevCheckingLinks => [...prevCheckingLinks, index]);
         }
 
-        // Через 5 секунд удаляем класс checking и добавляем класс checked
+        // Через 10 секунд удаляем класс checking и добавляем класс checked
         setTimeout(() => {
             setCheckingLinks(prevCheckingLinks => prevCheckingLinks.filter(i => i !== index));
             if (!checkedLinks.includes(index)) {
-                const newCheckedLinks = [...checkedLinks, index];
-                setCheckedLinks(newCheckedLinks);
-
-                // Сохраняем состояние в LocalStorage
-                localStorage.setItem('checkedLinks', JSON.stringify(newCheckedLinks));
+                setCheckedLinks(prevCheckedLinks => [...prevCheckedLinks, index]);
             }
-        }, 5000); // 5000 миллисекунд = 5 секунд
+        }, 10000); // 10000 миллисекунд = 10 секунд
     };
+
 
     return (
         <div className={`page-social ${className}`}>
@@ -60,7 +52,7 @@ const PageSocial = ({ className }) => {
             </a>
             <a
                 className={`social-item ${checkingLinks.includes(2) ? 'checking' : ''} ${checkedLinks.includes(2) ? 'checked' : ''}`}
-                href="https://www.youtube.com"
+                href="https://www.youtube.com/watch?v=H8t5uq-DRJk&list=RDEogFdxEzNcQ&index=12"
                 target="_blank"
                 rel="noopener noreferrer"
                 onClick={(e) => handleLinkClick(2, e)}
@@ -74,7 +66,7 @@ const PageSocial = ({ className }) => {
                 rel="noopener noreferrer"
                 onClick={(e) => handleLinkClick(3, e)}
             >
-                Web3
+                Web
             </a>
             <a
                 className={`social-item ${checkingLinks.includes(4) ? 'checking' : ''} ${checkedLinks.includes(4) ? 'checked' : ''}`}
@@ -87,7 +79,7 @@ const PageSocial = ({ className }) => {
             </a>
             <a
                 className={`social-item ${checkingLinks.includes(5) ? 'checking' : ''} ${checkedLinks.includes(5) ? 'checked' : ''}`}
-                href="https://twitter.com"
+                href="https://x.com"
                 target="_blank"
                 rel="noopener noreferrer"
                 onClick={(e) => handleLinkClick(5, e)}
