994de1a6ca844c68889c55a5552f679462064e18
OgurecA <acpwork044@gmail.com>
2024-08-09 11:51:37 +0400

    m

--- src/Components/PageSocial/PageSocial.jsx
+++ src/Components/PageSocial/PageSocial.jsx
@@ -11,6 +11,10 @@ const PageSocial = ({ className }) => {
         // Открываем ссылку сразу
         window.open(event.target.href, '_blank');
 
+        if (checkedLinks.includes(index)) {
+            return; // Если класс checked уже есть, выходим из функции
+        }
+
         // Добавляем класс checking
         if (!checkingLinks.includes(index)) {
             setCheckingLinks(prevCheckingLinks => [...prevCheckingLinks, index]);
