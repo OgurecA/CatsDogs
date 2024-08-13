f311e21f11c15743a0c5e186832adde615f9b2de
OgurecA <acpwork044@gmail.com>
2024-08-13 16:17:17 +0400

    k

--- src/Components/PageShop/PageShop.jsx
+++ src/Components/PageShop/PageShop.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './PageShop.css';
 
-const PageShop = ({ className, title }) => {
+const PageShop = ({ className, title, votesA, votesB }) => {
+    const [displayedVotes, setDisplayedVotes] = useState("Calculating...")
+    if (title === 'Dire Warriors') {
+        setDisplayedVotes(votesA);
+      } else if (title === 'Wild Hearts') {
+        setDisplayedVotes(votesB);
+      }
     return (
         <div className={`page-shop ${className}`}>
             <div className="shop-title">
                 {title}
             </div>
             <ul className="shop-list">
-                <li className="shop-item">
-                    <span>Item 1</span>
+                <li className="list-item">
+                    <span>Team Score: {displayedVotes}</span>
                 </li>
-                <li className="shop-item">
+                <li className="list-item">
                     <span>Item 2</span>
                 </li>
-                <li className="shop-item">
+                <li className="list-item">
                     <span>Item 3</span>
                 </li>
             </ul>
