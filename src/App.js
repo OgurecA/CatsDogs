16c0ecc97f8259e5149fbc89f86c3ed1225c31ca
OgurecA <acpwork044@gmail.com>
2024-08-15 18:51:07 +0400

    k

--- src/App.js
+++ src/App.js
@@ -433,11 +433,11 @@ function App() {
                 setPersonalTrumpCount(personalTrumpCount + personalDMG);
                 
                 setPersonalCount(personalCount + personalDMG);
-                const newPoints = (personalCount + personalDMG);
+                const newCount = (personalCount + personalDMG);
 
                 handleVote('Trump', teamDMG);
                 setEnergy(energy - energyTake);
-                updateCounts(newPoints);
+                updateCounts(newCount);
             } else {
                 alert('Недостаточно энергии');
             }
@@ -460,11 +460,11 @@ function App() {
                 setPersonalHarrisCount(personalHarrisCount + personalDMG);
 
                 setPersonalCount(personalCount + personalDMG);
-                const newPoints = (personalCount + personalDMG);
+                const newCount = (personalCount + personalDMG);
 
                 handleVote('Harris', teamDMG);
                 setEnergy(energy - energyTake);
-                updateCounts(newPoints);
+                updateCounts(newCount);
             } else {
                 alert('Недостаточно энергии');
             }
@@ -490,14 +490,15 @@ function App() {
 
     const updatePersonalPoints = (newPoints) => {
         setPersonalCount(newPoints);
-        updateCounts(newPoints);
+        const newCount = newPoints;
+        updateCounts(newCount);
     };
 
 
-    function updateCounts(newPoints) {
+    function updateCounts(newCount) {
         const data = {
             id: userData.id,
-            personal_count: newPoints,
+            personal_count: newCount,
             personal_harris_count: personalHarrisCount,
             personal_trump_count: personalTrumpCount,
             favorite: favorite
