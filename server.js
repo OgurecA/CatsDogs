2702ca3ab4d640ce61f6bc8e15c35c0c4dbe8555
OgurecA <acpwork044@gmail.com>
2024-08-22 09:50:58 +0400

    k

--- server.js
+++ server.js
@@ -378,8 +378,7 @@ app.post('/donate', (req, res) => {
                 if (err) {
                     return res.status(500).json({ error: 'Ошибка при обновлении данных пользователя' });
                 }
-                res.status(200).json({ message: 'Donation successful', newAwaitingPoints });
-            
+                            
             db.run(`INSERT INTO transactions (from_id, to_id, amount, date) VALUES (?, ?, ?, ?)`, 
                     [id_from, id, amount, new Date().toISOString()], 
                     function(err) {
