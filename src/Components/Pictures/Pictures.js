3ec009d02cf4e8b693edd142e846e45917e95b15
OgurecA <acpwork044@gmail.com>
2024-08-31 15:42:04 +0400

    k

--- src/Components/Pictures/Pictures.js
+++ src/Components/Pictures/Pictures.js
@@ -1,17 +1,8 @@
-import HarrisImg from "../Photoes/OwlCoin.png";
-import TrumpImg from "../Photoes/SnakeCoin.png";
-import HarrisBG from "../Photoes/FishBack.jpg";
-import TrumpBG from "../Photoes/Skull2.jpg";
 import TrumpP from "../Photoes/Cats.jpg";
 import HarrisP from "../Photoes/Loli.jpg";
 import bybit from "../Photoes/bybit.jpg";
 import SkullFon from "../Photoes/FonSkull.jpeg";
-import Tiger from "../Photoes/Tiger.png";
-import Snake from "../Photoes/Snake.png";
-import Croc from "../Photoes/Croc.png";
-import Elephant from "../Photoes/Elephant.png";
 import Gorilla from "../Photoes/Monke.png";
-import Rhino from "../Photoes/Rhino.png"
 import Cage from "../Photoes/Cage.png";
 import Krisa from "../Photoes/Krisa.png"
 import Bik from "../Photoes/Bik.png"
@@ -22,5 +13,5 @@ import FonTap2 from "../Photoes/FonTap2.jpeg"
 
 
 
-export { Cage, TrumpImg, TrumpBG, HarrisBG, HarrisImg, HarrisP, TrumpP, bybit, SkullFon, Rhino, Bik, Krisa,
-Gorilla, Tiger, Snake, Croc, Elephant, FonTap1, FonTap2 };
+export { Cage, HarrisP, TrumpP, bybit, SkullFon, Rhino, Bik, Krisa,
+Gorilla, FonTap1, FonTap2 };
