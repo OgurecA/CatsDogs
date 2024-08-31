93d7765b59f2b3fb0235633f72eacf9b212470ea
OgurecA <acpwork044@gmail.com>
2024-08-31 15:43:09 +0400

    k

--- src/Components/Pictures/Pictures.js
+++ src/Components/Pictures/Pictures.js
@@ -8,10 +8,9 @@ import Krisa from "../Photoes/Krisa.png"
 import Bik from "../Photoes/Bik.png"
 
 import FonTap1 from "../Photoes/FonTap1Dark.jpeg"
-import FonTap2 from "../Photoes/FonTap2.jpeg"
 
 
 
 
-export { Cage, HarrisP, TrumpP, bybit, SkullFon, Rhino, Bik, Krisa,
-Gorilla, FonTap1, FonTap2 };
+export { Cage, HarrisP, TrumpP, bybit, SkullFon, Bik, Krisa,
+Gorilla, FonTap1 };
