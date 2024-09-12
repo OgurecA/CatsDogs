3b64a579bd342f181876ad51162e0c030c50717c
OgurecA <acpwork044@gmail.com>
2024-09-12 11:29:53 +0400

    k

--- src/Components/PageShop/PageShop.jsx
+++ src/Components/PageShop/PageShop.jsx
@@ -50,7 +50,8 @@ const PageShop = ({ className, title, votesA, votesB, personalCount, contributio
     const promoCodes = [
         { code: "PROMO2024", points: 1000, start: new Date('2024-01-01'), expiry: new Date('2024-12-31') },
         { code: "SEPGIFT", points: 1000, start: new Date('2024-09-10'), expiry: new Date('2024-09-14') },
-        { code: "HAPPYNEWYEAR", points: 1000, start: new Date('2024-09-10'), expiry: new Date('2024-09-14') },
+        { code: "WELCOME", points: 500, start: new Date('2024-09-10'), expiry: new Date('2024-11-01') },
+        { code: "AUTUMN2024", points: 1000, start: new Date('2024-09-10'), expiry: new Date('2024-12-01') },
         // Добавьте больше промокодов по необходимости
     ];
     const [usedPromoCodes, setUsedPromoCodes] = useState([]);
